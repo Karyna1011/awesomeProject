777a1afbeaf782c912cd8be1cf4bae7fa6b61b8f
Pharos1011 <90761541+Phanos1011@users.noreply.github.com>
2021-10-07 14:49:02 +0300

    Add files via upload

--- config/main.go
+++ config/main.go
@@ -1,1 +1,30 @@
+package config
 
+import (
+	"gitlab.com/distributed_lab/kit/comfig"
+	"gitlab.com/distributed_lab/kit/kv"
+	"gitlab.com/distributed_lab/logan/v3"
+)
+
+var ERC20WithdrawVersion string
+
+type config struct {
+	getter kv.Getter
+	once   comfig.Once
+
+	Ether
+	comfig.Logger
+}
+
+type Config interface {
+	Log() *logan.Entry
+	Ether
+}
+
+func NewConfig(getter kv.Getter) Config {
+	return &config{
+		getter:    getter,
+		Ether:     NewEther(getter),
+		Logger:    comfig.NewLogger(getter, comfig.LoggerOpts{Release: ERC20WithdrawVersion}),
+	}
+}
